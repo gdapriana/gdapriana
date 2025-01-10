3d5b059d158e71d6791ded48134c581e51966142
gedeapriana <gedeapriana36@gmail.com>
2025-01-11 07:32:41 +0800

    services page config

---
+++ app/(root)/_components/service.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function Service() {
+  return (
+    <main className="flex flex-col justify-center items-stretch">
+      <header className="flex md:px-20 md:py-20 p-8 gap-8 justify-start items-center">
+        <h1 className="font-bold text-4xl text-primary-foreground">Services and Capabilities</h1>
+        <div className="h-[1px] flex-1 bg-stone-800"></div>
+      </header>
+    </main>
+  )
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/app/(root)/_components/hero";
 import About from "@/app/(root)/_components/about";
 import Work from "@/app/(root)/_components/work";
+import Service from "@/app/(root)/_components/service";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Work />
+      <Service />
     </div>
   );
 }
