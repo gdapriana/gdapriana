bbf377ebdaf248e4ad7452e0e988b8d94d666639
gedeapriana <gedeapriana36@gmail.com>
2025-01-10 18:10:19 +0800

    edit next config

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,9 @@
 'use client'
 
-import {ReactNode, useEffect} from "react";
+import {ReactNode} from "react";
 import Header from "@/app/(root)/_components/header";
-import Lenis from "lenis";
 
 export default function Layout({children}: { children: ReactNode }) {
-
-  useEffect(() => {
-    const lenis = new Lenis()
-    const raf = (time: any) => {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-    requestAnimationFrame(raf);
-  }, [])
 
   return (
     <main className="h-screen bg-primary w-full flex justify-center items-center flex-col">
