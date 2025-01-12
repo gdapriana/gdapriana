07a633924239183b6b67d7df4dc8c29b66b37bf9
gedeapriana <gedeapriana36@gmail.com>
2025-01-12 11:38:30 +0800

    working on contact section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
 import Hero from "@/app/(root)/_components/hero";
 import About from "@/app/(root)/_components/about";
 import Work from "@/app/(root)/_components/work";
-import Service from "@/app/(root)/_components/service";
 import Experience from "@/app/(root)/_components/experience";
+import Contact from "@/app/(root)/_components/contact";
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
       <About />
       <Work />
       <Experience />
-      <Service />
+      <Contact />
     </div>
   );
 }
